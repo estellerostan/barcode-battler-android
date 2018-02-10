57d3def058d1173d33ba55f59db7b95c72d4f9c4
Michael Gianfaldone <gmitch.dev@outlook.fr>
2018-02-10 23:19:36 +0100

    modification de l'activité de servant de point de départ

--- app/src/main/java/org/mbds/barcodebattler/MainActivity.java
+++ app/src/main/java/org/mbds/barcodebattler/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import org.mbds.barcodebattler.battle.BattleActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button generateButton;
     private Button toPoolButton;
+    private Button toBattleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         generateButton = (Button) findViewById(R.id.generate);
         toPoolButton = (Button) findViewById(R.id.toPool);
+        toBattleButton = (Button) findViewById(R.id.toBattle);
 
         generateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CreaturesPoolActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toBattleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BattleActivity.class);
                 startActivity(intent);
             }
         });
