d9d7c84ea826781fcd4da137138659e84ba31568
Estelle Rostan <estelle.rostan@etu.unice.fr>
2018-02-10 20:25:55 +0100

    fix #7 Ajoute le chargement de Bitmaps larges de maniere efficace
    ref.: https://developer.android.com/topic/performance/graphics/load-bitmap.html

--- app/src/main/java/org/mbds/barcodebattler/Data.java
+++ app/src/main/java/org/mbds/barcodebattler/Data.java
@@ -1,6 +1,7 @@
 package org.mbds.barcodebattler;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.DataSetObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -58,6 +59,45 @@ class Data implements ListAdapter {
 //        superheroes.add(s4);
 //        superheroes.add(s5);
 //        superheroes.add(s6);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 
     @Override
@@ -125,10 +165,15 @@ class Data implements ListAdapter {
         String mDrawableName = superheroes.get(position).getImageName();
         int resID = returnView.getResources().getIdentifier(mDrawableName, "drawable", returnView.getContext().getPackageName());
 
-        Bitmap icon = BitmapFactory.decodeResource(returnView.getContext().getResources(),
-                resID);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(returnView.getContext().getResources(), resID, options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
 
-        image.setImageBitmap(icon);
+        image.setImageBitmap(
+                decodeSampledBitmapFromResource(returnView.getContext().getResources(), resID, 100, 100));
 
         return returnView;
     }
